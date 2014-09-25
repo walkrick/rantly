6ff01a027d11975a2556d9964f6941f0041de4a5
Rick Walker <walkrick@yahoo.com>
2014-09-25 11:19:22 -0600

    added root link to title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_parameters)
+      flash[:notice] = "Profile was updated successfully"
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def create
