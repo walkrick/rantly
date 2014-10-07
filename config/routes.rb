31174e4d999748c74767ed0e6403655ea6706d4f
Rick Walker <walkrick@yahoo.com>
2014-10-07 16:06:33 -0600

    migrated and fix on edit form

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :rants
+
   end
 
   resources :dashboard
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+# User.create(username: 'walkrick', password_digest: 'password', first_name: 'Rick', last_name: 'Walker', bio: 'Something', frequency: 'Weekly')
