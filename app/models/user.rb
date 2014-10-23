e87411659d0255dee313436b604edea12e38be45
Rick Walker <walkrick@yahoo.com>
2014-10-23 14:12:48 -0600

    added password length verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
 
   validates :username, presence: true, uniqueness: {case_sensitive: false}
-  validates :password_digest, presence: true
+  validates :password_digest, presence: true, length: { in: 8..20 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true
