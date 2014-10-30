44f8f59552457b466b140a16b0f2c3daa0c682cb
Rick Walker <walkrick@yahoo.com>
2014-10-30 10:57:46 -0600

    Formatted search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
     @user_results = User.where("first_name LIKE :first_name or last_name LIKE :last_name or username LIKE :username" , {first_name: "%#{params[:query]}%", last_name: "%#{params[:query]}%", username: "%#{params[:query]}%" })
     @rant_results = Rant.where("rant LIKE :rant" , {rant: "%#{params[:query]}%" })
     @rant = Rant.new
+    @rants = Rant.where(user_id: params[:id])
+
   end
 
 
